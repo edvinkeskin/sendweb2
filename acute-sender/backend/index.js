32dd7a24984605f66a9ec29fd634d0703ecb1955
edvin <edvinkeskin@gmail.com>
2022-07-09 21:43:33 +0300

    mongodb first attempt progress

---
+++ acute-sender/backend/index.js
@@ -0,0 +1,32 @@
+const {MongoClient} = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://edvinkeskin:Istanbul4045@acutesender20.e0ds5.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
