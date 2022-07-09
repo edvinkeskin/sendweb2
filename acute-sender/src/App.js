56b8ae1bec0b00540533533e1a979e8fd70dbce1
edvin <edvinkeskin@gmail.com>
2022-07-09 16:13:18 +0300

    mongodb first attempt

--- acute-sender/src/App.js
+++ acute-sender/src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+const {MongoClient} = require('mongodb');
+
+
+
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://edvinkeskin:Istanbul4045@acutesender20.e0ds5.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
